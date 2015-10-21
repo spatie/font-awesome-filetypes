4f8d3a275c531348081e13864536746267cdc750
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-10-21 12:28:36 +0200

    Test refactor

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import icons from './icons'
  * @param {string} extension
  * @returns {string}
  */
-function getClassNameForExtension(extension) {
+export default function getClassNameForExtension(extension) {
     return extensions[extension.toLowerCase()] || icons.file
 }
 
-export default getClassNameForExtension
+/**
+ * @param {string} extension
+ * @returns {string}
+ */
+export function getIconForExtension(extension) {
+    return `<i class="fa ${getClassNameForExtension(extension)}"></i>`
+}
